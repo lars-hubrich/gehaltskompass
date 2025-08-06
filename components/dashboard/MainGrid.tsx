356d1682af37394bedf0aee1e0fad8112a9c8070
Jan Lieder <jlzmk56@gmail.com>
2025-08-06 13:00:33 +0200

    remove useless charts and views

--- components/dashboard/MainGrid.tsx
+++ components/dashboard/MainGrid.tsx
@@ -1,11 +1,8 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Copyright from "../internals/components/Copyright";
-import ChartUserByCountry from "./ChartUserByCountry";
-import CustomizedTreeView from "./CustomizedTreeView";
 import CustomizedDataGrid from "./CustomizedDataGrid";
 import HighlightedCard from "./HighlightedCard";
 import PageViewsBarChart from "./PageViewsBarChart";
@@ -80,12 +77,6 @@ export default function MainGrid() {
       <Grid container spacing={2} columns={12}>
         <Grid size={{ xs: 12, lg: 9 }}>
           <CustomizedDataGrid />
-        </Grid>
-        <Grid size={{ xs: 12, lg: 3 }}>
-          <Stack gap={2} direction={{ xs: "column", sm: "row", lg: "column" }}>
-            <CustomizedTreeView />
-            <ChartUserByCountry />
-          </Stack>
         </Grid>
       </Grid>
       <Copyright sx={{ my: 4 }} />
