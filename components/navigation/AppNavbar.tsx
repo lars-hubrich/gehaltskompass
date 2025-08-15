8c838b57e458bb888c873ca7d5f526b5ae8ed958
Jan Lieder <jlzmk56@gmail.com>
2025-08-15 13:32:28 +0200

    remove unnecessary icon and fixed title switching in AppNavbar

--- components/navigation/AppNavbar.tsx
+++ components/navigation/AppNavbar.tsx
@@ -1,16 +1,15 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import MuiToolbar from "@mui/material/Toolbar";
 import { tabsClasses } from "@mui/material/Tabs";
 import Typography from "@mui/material/Typography";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
-import DashboardRoundedIcon from "@mui/icons-material/DashboardRounded";
 import SideMenuMobile from "./SideMenuMobile";
 import MenuButton from "./MenuButton";
 import ColorModeIconDropdown from "@/theme/ColorModeIconDropdown";
+import { usePathname } from "next/navigation";
 
 const Toolbar = styled(MuiToolbar)({
   width: "100%",
@@ -35,6 +34,18 @@ const Toolbar = styled(MuiToolbar)({
  */
 export default function AppNavbar() {
   const [open, setOpen] = React.useState(false);
+
+  const pathname = usePathname();
+  const title = React.useMemo(() => {
+    if (pathname === "/dashboard") {
+      return "Übersicht";
+    } else if (pathname === "/statements") {
+      return "Abrechnungen";
+    } else if (pathname === "/insights") {
+      return "AI Insights";
+    }
+    return undefined;
+  }, [pathname]);
 
   /**
    * Returns a handler to toggle the mobile side drawer.
@@ -69,20 +80,13 @@ export default function AppNavbar() {
             gap: 1,
           }}
         >
-          <Stack
-            direction="row"
-            spacing={1}
-            sx={{ justifyContent: "center", mr: "auto" }}
+          <Typography
+            variant="h4"
+            component="h1"
+            sx={{ color: "text.primary" }}
           >
-            <CustomIcon />
-            <Typography
-              variant="h4"
-              component="h1"
-              sx={{ color: "text.primary" }}
-            >
-              Übersicht
-            </Typography>
-          </Stack>
+            {title}
+          </Typography>
           <ColorModeIconDropdown />
           <MenuButton aria-label="Menü" onClick={toggleDrawer(true)}>
             <MenuRoundedIcon />
@@ -91,35 +95,5 @@ export default function AppNavbar() {
         </Stack>
       </Toolbar>
     </AppBar>
-  );
-}
-
-/**
- * Small dashboard icon used in the navigation bar.
- *
- * @returns {JSX.Element} Custom icon.
- */
-export function CustomIcon() {
-  return (
-    <Box
-      sx={{
-        width: "1.5rem",
-        height: "1.5rem",
-        bgcolor: "black",
-        borderRadius: "999px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        alignSelf: "center",
-        backgroundImage:
-          "linear-gradient(135deg, hsl(210, 98%, 60%) 0%, hsl(210, 100%, 35%) 100%)",
-        color: "hsla(210, 100%, 95%, 0.9)",
-        border: "1px solid",
-        borderColor: "hsl(210, 100%, 55%)",
-        boxShadow: "inset 0 2px 5px rgba(255, 255, 255, 0.3)",
-      }}
-    >
-      <DashboardRoundedIcon color="inherit" sx={{ fontSize: "1rem" }} />
-    </Box>
   );
 }
